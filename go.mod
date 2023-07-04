6c103491afec511df4e403ecabf65d5a83f85013
Martin Montes <martin11lrx@gmail.com>
2023-07-04 22:05:35 +0200

    Get rid of cobra in favor of flag

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/go-chi/httprate v0.7.4
 	github.com/go-logr/logr v1.2.4
 	github.com/google/uuid v1.3.0
-	github.com/spf13/cobra v1.7.0
 	go.uber.org/zap v1.24.0
 	k8s.io/apimachinery v0.24.2
 	sigs.k8s.io/controller-runtime v0.12.3
@@ -18,11 +17,9 @@ require (
 	github.com/go-logr/zapr v1.2.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
--- main.go
+++ main.go
@@ -1,7 +1,94 @@
 package main
 
-import "github.com/mariadb-operator/agent/cmd/agent"
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/mariadb-operator/agent/pkg/filemanager"
+	"github.com/mariadb-operator/agent/pkg/handler"
+	"github.com/mariadb-operator/agent/pkg/logger"
+	"github.com/mariadb-operator/agent/pkg/router"
+	"github.com/mariadb-operator/agent/pkg/server"
+)
+
+var (
+	addr      string
+	configDir string
+	stateDir  string
+
+	compressLevel     int
+	rateLimitRequests int
+	rateLimitDuration time.Duration
+	gracefulShutdown  time.Duration
+
+	logLevel       string
+	logTimeEncoder string
+	logDev         bool
+
+	recoveryTimeout time.Duration
+)
 
 func main() {
-	agent.Execute()
+	flag.StringVar(&addr, "addr", ":5555", "The address that the HTTP server binds to")
+	flag.StringVar(&configDir, "config-dir", "/etc/mysql/mariadb.conf.d", "The directory that contains MariaDB configuration files")
+	flag.StringVar(&stateDir, "state-dir", "/var/lib/mysql", "The directory that contains MariaDB state files")
+
+	flag.IntVar(&compressLevel, "compress-level", 5, "HTTP compression level")
+	flag.IntVar(&rateLimitRequests, "rate-limit-requests", 0, "Number of requests to be used as rate limit")
+	flag.DurationVar(&rateLimitDuration, "rate-limit-duration", 0, "Duration to be used as rate limit")
+	flag.DurationVar(&gracefulShutdown, "graceful-shutdown", 5*time.Second, "Timeout to gracefully terminate "+
+		"in-flight requests")
+
+	flag.StringVar(&logLevel, "log-level", "info", "Log level to use, one of: "+
+		"debug, info, warn, error, dpanic, panic, fatal.")
+	flag.StringVar(&logTimeEncoder, "log-time-encoder", "epoch", "Log time encoder to use, one of: "+
+		"epoch, millis, nano, iso8601, rfc3339 or rfc3339nano")
+	flag.BoolVar(&logDev, "log-dev", false, "Enable development logs.")
+
+	flag.DurationVar(&recoveryTimeout, "recovery-timeout", 1*time.Minute, "Timeout to obtain sequence number "+
+		"during the Galera cluster recovery process")
+	flag.Parse()
+
+	logger, err := logger.NewLogger(
+		logger.WithLogLevel(logLevel),
+		logger.WithTimeEncoder(logTimeEncoder),
+		logger.WithDevelopment(logDev),
+	)
+	if err != nil {
+		log.Fatalf("error creating logger: %v", err)
+	}
+
+	fileManager, err := filemanager.NewFileManager(configDir, stateDir)
+	if err != nil {
+		logger.Error(err, "error creating file manager")
+		os.Exit(1)
+	}
+
+	handlerLogger := logger.WithName("handler")
+	handler := handler.NewHandler(
+		fileManager,
+		&handlerLogger,
+		handler.WithRecoveryTimeout(recoveryTimeout),
+	)
+
+	router := router.NewRouter(
+		handler,
+		router.WithCompressLevel(compressLevel),
+		router.WithRateLimit(rateLimitRequests, rateLimitDuration),
+	)
+
+	serverLogger := logger.WithName("server")
+	server := server.NewServer(
+		addr,
+		router,
+		&serverLogger,
+		server.WithGracefulShutdown(gracefulShutdown),
+	)
+	if err := server.Start(context.Background()); err != nil {
+		logger.Error(err, "server error")
+		os.Exit(1)
+	}
 }
